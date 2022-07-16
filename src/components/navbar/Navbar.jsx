9d9f82f05929df1142efce6fd168e99242187fb3
boriscript <boris.bdc@gmail.com>
2022-07-16 16:03:03 -0300

    integrating Sidebar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,32 @@
 import "./navbar.scss";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import PublicOutlinedIcon from "@mui/icons-material/PublicOutlined";
+import NightlightOutlinedIcon from '@mui/icons-material/NightlightOutlined';
+import NightlightRoundOutlinedIcon from '@mui/icons-material/NightlightRoundOutlined';
+
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        Navbar Wrapper
+        <div className="search">
+          search bar
+          <input type="text" placeholder="Search..." />
+          <SearchOutlinedIcon className="icon" />
+        </div>
+        <div className="items">
+          <div className="item">
+            <PublicOutlinedIcon className="icon" />
+            English
+          </div>
+          <div className="item">
+            <NightlightOutlinedIcon className="icon" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,16 @@
 import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import CategoryOutlinedIcon from "@mui/icons-material/CategoryOutlined";
+import InventoryOutlinedIcon from "@mui/icons-material/InventoryOutlined";
+import LocalShippingOutlinedIcon from "@mui/icons-material/LocalShippingOutlined";
+import QueryStatsOutlinedIcon from "@mui/icons-material/QueryStatsOutlined";
+import CircleNotificationsOutlinedIcon from "@mui/icons-material/CircleNotificationsOutlined";
+import SystemSecurityUpdateGoodOutlinedIcon from "@mui/icons-material/SystemSecurityUpdateGoodOutlined";
+import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
+import SettingsApplicationsOutlinedIcon from "@mui/icons-material/SettingsApplicationsOutlined";
+import ManageAccountsOutlinedIcon from "@mui/icons-material/ManageAccountsOutlined";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 
 const Sidebar = () => {
   return (
@@ -8,29 +20,66 @@ const Sidebar = () => {
       </div>
       <hr />
       <div className="center">
-        list
         <ul>
+          <p className="title">Vamo River, vieja</p>
           <li>
+            <DashboardIcon className="icon" />
             <span>Dashboard</span>
           </li>
+          <p className="title">Lists</p>
           <li>
-            <span>Dashboard</span>
+            <AccountCircleRoundedIcon className="icon" />
+            <span>Users</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <CategoryOutlinedIcon className="icon" />
+            <span>Products</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <InventoryOutlinedIcon className="icon" />
+            <span>Orders</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <LocalShippingOutlinedIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">useful</p>
+          <li>
+            <QueryStatsOutlinedIcon className="icon" />
+            <span>Stats</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <CircleNotificationsOutlinedIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+          <p className="title">service</p>
+          <li>
+            <SystemSecurityUpdateGoodOutlinedIcon className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyOutlinedIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsApplicationsOutlinedIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">user</p>
+          <li>
+            <ManageAccountsOutlinedIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutOutlinedIcon className="icon" />
+            <span>Log Out</span>
           </li>
         </ul>
       </div>
-      <div className="bottom">color options</div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
     </div>
   );
 };
