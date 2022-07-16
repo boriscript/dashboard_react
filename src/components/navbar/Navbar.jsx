fa67e0cccb08b3e957818eb84245378e73d6bec5
boriscript <boris.bdc@gmail.com>
2022-07-16 16:33:57 -0300

    Navbar component mockup finished

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,19 @@
 import "./navbar.scss";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import PublicOutlinedIcon from "@mui/icons-material/PublicOutlined";
-import NightlightOutlinedIcon from '@mui/icons-material/NightlightOutlined';
-import NightlightRoundOutlinedIcon from '@mui/icons-material/NightlightRoundOutlined';
+import NightlightOutlinedIcon from "@mui/icons-material/NightlightOutlined";
+import CropFreeOutlinedIcon from '@mui/icons-material/CropFreeOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined';
+/* import NightlightRoundOutlinedIcon from "@mui/icons-material/NightlightRoundOutlined"; */
 
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="wrapper">
-        Navbar Wrapper
         <div className="search">
-          search bar
           <input type="text" placeholder="Search..." />
           <SearchOutlinedIcon className="icon" />
         </div>
@@ -22,6 +24,23 @@ const Navbar = () => {
           </div>
           <div className="item">
             <NightlightOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <CropFreeOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className="icon" />
+            <div className="counter">3</div>
+          </div>
+          <div className="item">
+            <FormatListBulletedOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <img className="avatar" src="https://thumbs.dreamstime.com/b/darth-vader-flat-icon-star-wars-vector-illustration-isolated-white-space-character-gradient-style-design-designed-darth-vader-174034014.jpg" alt="user avatar" />
           </div>
         </div>
       </div>
