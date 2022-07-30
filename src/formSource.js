d1404cf9ff7ff93db6c651ac520ee5df2c430aa8
boriscript <boris.bdc@gmail.com>
2022-07-30 13:28:27 -0300

    finish mockup for new products

---
+++ src/formSource.js
@@ -0,0 +1,76 @@
+export const userInputs = [
+  {
+    id: 1,
+    label: "Username",
+    type: "text",
+    placeholder: "juan_doe",
+  },
+  {
+    id: 2,
+    label: "Name Surname",
+    type: "text",
+    placeholder: "Juan Doe",
+  },
+  {
+    id: 3,
+    label: "Email",
+    type: "text",
+    placeholder: "juan_doe@gmail.com",
+  },
+  {
+    id: 4,
+    label: "Phone",
+    type: "text",
+    placeholder: "+54 9 15 1234-6789",
+  },
+  {
+    id: 5,
+    label: "Password",
+    type: "password",
+  },
+  {
+    id: 6,
+    label: "Address",
+    type: "text",
+    placeholder: "Calle Falsa 123",
+  },
+  {
+    id: 7,
+    label: "Country",
+    type: "text",
+    placeholder: "Argentine",
+  },
+];
+
+export const productInputs = [
+  {
+    id: 1,
+    label: "Title",
+    type: "text",
+    placeholder: "Apple Macbook Pro",
+  },
+  {
+    id: 2,
+    label: "Description",
+    type: "text",
+    placeholder: "Description",
+  },
+  {
+    id: 3,
+    label: "Category",
+    type: "text",
+    placeholder: "Computers",
+  },
+  {
+    id: 4,
+    label: "Price",
+    type: "text",
+    placeholder: "100",
+  },
+  {
+    id: 5,
+    label: "Stock",
+    type: "text",
+    placeholder: "in stock",
+  },
+];
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,10 +1,66 @@
 import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import DriveFolderUploadOutLinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import "./new.scss";
 
-const New = () => {
+const New = ({ inputs, title }) => {
   return (
-    <div className="home">
-      <h1 className="title">Dashboard New</h1>
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1>{title}</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img
+              src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              alt="Empty avatar"
+            />
+          </div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label htmlFor="file">
+                  Image: <DriveFolderUploadOutLinedIcon className="icon" />
+                </label>
+                <input type="file" id="file" style={{ display: "none" }} />
+              </div>
+              <div className="formInput">
+                <label>Username</label>
+                <input type="text" placeholder="juana_doe" />
+              </div>
+              <div className="formInput">
+                <label>Name & Surname</label>
+                <input type="text" placeholder="Juana Doe" />
+              </div>
+              <div className="formInput">
+                <label>Email</label>
+                <input type="email" placeholder="juana_doe@gmail.com" />
+              </div>
+              <div className="formInput">
+                <label>Phone</label>
+                <input type="text" placeholder="+54 9 15 1234-6789" />
+              </div>
+              <div className="formInput">
+                <label>Password</label>
+                <input type="password" />
+              </div>
+              <div className="formInput">
+                <label>Address</label>
+                <input type="text" placeholder="Calle Falsa 123" />
+              </div>
+              <div className="formInput">
+                <label>Country</label>
+                <input type="text" placeholder="Argentine" />
+              </div>
+              <button>Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
